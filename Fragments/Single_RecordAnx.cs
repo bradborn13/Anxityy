4f0757de67219351aebcc4937ac2ec0b9fa49df1
Lucian <bradborn13@yahoo.com>
2020-08-09 16:00:06 +0200

    refactored single_anx

--- Fragments/Single_RecordAnx.cs
+++ Fragments/Single_RecordAnx.cs
@@ -6,7 +6,9 @@ using Android.Graphics;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
+using Java.Lang;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Xamarin.Forms.Platform.Android;
@@ -31,20 +33,30 @@ namespace Anxityy.Fragments
             View view = inflater.Inflate(Resource.Layout.singleRecordAnx, container, false);
             LinearLayout anxLayout = view.FindViewById<LinearLayout>(Resource.Id.anxLayout);
             SetupMap();
+            LinearLayout locationLayout = view.FindViewById<LinearLayout>(Resource.Id.locationLayout);
 
-
-
+            View fragMap = view.FindViewById<View>(Resource.Id.map);
+            TextView locationTextView = view.FindViewById<TextView>(Resource.Id.locationTextView);
+            TextView dateTextView = view.FindViewById<TextView>(Resource.Id.dateTextView);
+            TextView seekbarTextView = view.FindViewById<TextView>(Resource.Id.seekbar_val);
+            SeekBar seekbar = view.FindViewById<SeekBar>(Resource.Id.seekbar);
             getAnxRecord();
-            foreach (var val in anx.GetType().GetProperties())
+            dateTextView.Text = anx.date;
+            seekbarTextView.Text = Convert.ToString(anx.rating);
+            seekbar.Progress = anx.rating;
+            seekbar.Touch += (s, e) =>
             {
-                if (val.Name == "_id")
-                {
-                    continue;
-                }
-                var formLayout = createAnxLayout(val, anx);
-                anxLayout.AddView(formLayout);
-            }
+            };
+            seekbar.Enabled = false;
+            if (string.IsNullOrEmpty(anx.locationLat) || string.IsNullOrEmpty(anx.locationLong))
+            {
+                fragMap.Visibility = ViewStates.Invisible;
+                locationLayout.Visibility = ViewStates.Invisible;
+            } else {
+                locationTextView.Text = anx.locationName;
 
+            };
+        
             return view;
         }
 
@@ -57,64 +69,25 @@ namespace Anxityy.Fragments
                 SupportMapFragment mapFragment = ChildFragmentManager.FindFragmentById(Resource.Id.map) as SupportMapFragment;
                 mapFragment.GetMapAsync(this);
             }
-
         }
-        public RelativeLayout createAnxLayout(PropertyInfo val, AnxityRecords anx)
-        {
-            RelativeLayout layout_lin1 = new RelativeLayout(Activity);
 
-            RelativeLayout.LayoutParams lin1_params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MatchParent, RelativeLayout.LayoutParams.MatchParent);
-            RelativeLayout.LayoutParams lin1_params_left = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WrapContent, RelativeLayout.LayoutParams.WrapContent);
-            RelativeLayout.LayoutParams lin1_params_right = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WrapContent, RelativeLayout.LayoutParams.WrapContent);
-
-            RelativeLayout layout_lin1_left = new RelativeLayout(Context);
-            RelativeLayout layout_lin1_right = new RelativeLayout(Context);
-
-            layout_lin1.LayoutParameters = lin1_params;
-            layout_lin1_left.LayoutParameters = lin1_params_left;
-            layout_lin1_right.LayoutParameters = lin1_params_right;
-            lin1_params_right.AddRule(LayoutRules.AlignParentRight);
-            lin1_params_left.AddRule(LayoutRules.AlignParentLeft);
-
-            TextView textView = new TextView(Activity);
-            textView.Text = "" + FirstCharToUpper(val.Name);
-            textView.SetTypeface(Typeface.Serif, TypefaceStyle.Bold);
-            textView.SetPadding(60, 50, 0, 50);
-            layout_lin1_left.AddView(textView);
-            TextView textView2 = new TextView(Activity);
-            textView2.Text = "" + val.GetValue(anx);
-            textView2.SetTypeface(Typeface.Serif, TypefaceStyle.Bold);
-            textView2.SetPadding(0, 50, 50, 50);
-            layout_lin1_right.AddView(textView2);
-            layout_lin1.AddView(layout_lin1_left);
-            layout_lin1.AddView(layout_lin1_right);
-
-
-            return layout_lin1;
-        }
         public void getAnxRecord()
         {
             int anxId = Arguments.GetInt("recordId");
 
             anx = new AnxityDatabase().GetAnxityRecordAsync(anxId).Result;
         }
-        public static string FirstCharToUpper(string input)
-        {
-            if (String.IsNullOrEmpty(input))
-                throw new ArgumentException("ARGH!");
-            return input.First().ToString().ToUpper() + input.Substring(1);
-        }
 
-        public  void OnMapReady(GoogleMap googleMap)
+        public void OnMapReady(GoogleMap googleMap)
         {
-           
-            if(anx.locationLat != null && anx.locationLong != null)
+
+            if (anx.locationLat != null && anx.locationLong != null)
             {
                 mMap = googleMap;
                 mMap.UiSettings.ZoomControlsEnabled = true;
                 mMap.UiSettings.CompassEnabled = true;
                 mMap.UiSettings.ZoomControlsEnabled = true;
-                LatLng marker = new LatLng(Convert.ToDouble( anx.locationLat), Convert.ToDouble(anx.locationLong));
+                LatLng marker = new LatLng(Convert.ToDouble(anx.locationLat), Convert.ToDouble(anx.locationLong));
                 CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(marker, 15);
                 mMap.MoveCamera(camera);
                 var markerOptions = new MarkerOptions().SetPosition(marker).SetTitle("Target 0 ");
@@ -132,7 +105,7 @@ namespace Anxityy.Fragments
                 //var markerOptions = new MarkerOptions().SetPosition(marker).SetTitle("Target 0 ");
                 //mMap.AddMarker(markerOptions);
             }
-           
+
 
 
         }
